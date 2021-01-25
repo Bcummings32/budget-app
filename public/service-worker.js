725f8f186c9c0a1635ff678c31e96849d375bb76
Ben Cummings <bncmmngs@aol.com>
2021-01-25 13:40:44 -0500

    service-worker.js edit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,77 +1,80 @@
-const { cache } = require("webpack");
-
-
-const FILES_TO_CACHE = [
-    "db.js",
-    "/index.html",
-    "/index.js",
-    "/style.css",
-    "/manifest.webmanifest",
-    "/favicon.ico",
-    "/assets/images/icons/icon-72x72.png",
-    "/assets/images/icons/icon-96x96.png",
-    "/assets/images/icons/icon-128x128.png",
-    "/assets/images/icons/icon-144x144.png",
-    "/assets/images/icons/icon-152x152.png",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-384x384.png",
-    "/assets/images/icons/icon-512x512.png"
-]
-
-const CACHE_NAME = "static-cache-v1", DATA_CACHE_NAME = "data-cache-v1";
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+var FILES_TO_CACHE = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
 
 //set up event listener
 self.addEventListener("instal", function(event) {
+
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             //Then pre-cache all of the files to cache. (Pre-caching is done in anticipation)
             console.log("Your files were pre-cahced successfully!");
             return cache.addAll(FILES_TO_CACHE);
             self.skipWaiting();
-});
-
-        })
-});
-
-self.addEventListener("activate", (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-        caches
-        .keys()
-        .then((cacheNames) =>
-        return cacheNames.filter(
-            (cacheName) => !currentCaches.includes(cacheName) 
-        );
-        )
 })
-.then((cachesToDelete) => {
-    return Promise.all
-    cachesToDelete.map((cacheToDelete) => {
-    return caches.delete(cacheToDelete);
-});
-}
+
 )
-.then(( => self.ClientRectList.claim())
-);
 });
 
-self.addEventListener("fetch", (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-            caches.match(event.request).then((cachedResponse) => {
-                if (cachedREsponse) {
-                    return.cachedResponse;
-                }
-                return caches.open(RUNTIME).then((cache) => {
-                    return fetch(event.request).then((response) => {
-                      return cache.put(event.request, response.clone()).then(() => {
-                        return response;
+// self.addEventListener("activate", (event) => {
+//     const currentCaches = [PRECACHE, RUNTIME];
+//     event.waitUntil(
+//         caches
+//         .keys()
+//         .then((cacheNames) =>
+//         return cacheNames.filter(
+//             (cacheName) => !currentCaches.includes(cacheName) 
+//         );
+//         )
+// })
+// .then((cachesToDelete) => {
+//     return Promise.all
+//     cachesToDelete.map((cacheToDelete) => {
+//     return caches.delete(cacheToDelete);
+// });
+// }
+// )
+// .then(( => self.ClientRectList.claim())
+// );
+// });
 
-            })
-        )
-    }
-}
-)
+self.addEventListener("fetch", (event) => {if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+    
+})
 
 
 
